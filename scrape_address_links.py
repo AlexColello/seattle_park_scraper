b949a9348ee8e7c064f6923dfa2aea6742c8193b
Alex Colello <alexlcolello@gmail.com>
2024-11-03 07:58:40 -0800

    Parse by name instead of id and handle 404 pages

--- scrape_address_links.py
+++ scrape_address_links.py
@@ -6,22 +6,50 @@ import requests
 import re
 from pprint import pprint
 import csv
+from enum import Enum
 
-missing_map_link = []
+
+class LinkResult(Enum):
+    Successful = "successful"
+    LinkMissing = "link_missing"
+    PageMissing = "page_missing"
 
 
-def get_maps_link_from_page(id):
-    url = f"https://seattle.gov/parks/allparks/?ID={id}"
-    r = requests.get(url)
+# https://www.seattle.gov/parks/allparks/parks-e-h?pageNum=1&itemsPer=1000&displayType=Thumbnail_Excerpt
+
+
+def get_park_container():
+    with open("data_url.txt", "r") as f:
+        data_url = f.read()
+
+    all_data = requests.get(data_url).json()
+    return all_data["features"]
+
+
+def get_park_info(raw_park_data):
+    park_name = raw_park_data["attributes"]["PMA_Name"]
+    park_id = raw_park_data["attributes"]["PMA"]
+
+    sanitized_name = park_name.lower().replace(" ", "-").replace(".", "")
+    url = f"http://seattle.gov/parks/allparks/{sanitized_name}"
+    return park_name, park_id, url
+
+
+def get_maps_link_from_page(url: str):
+    r = requests.get(url, verify=False)
+    if r.status_code != 200:
+        print(f"Page returned unsuccessful status code {r.status_code}")
+        return None, LinkResult.PageMissing
+
     soup = BeautifulSoup(r.content, features="html.parser")
 
     if len(soup.find_all("script")) == 1:
-        return None
+        return None, LinkResult.PageMissing
 
     if soup.find(string="The park page has moved"):
         redirect_name = soup.a["href"]
         url = f"https://seattle.gov/parks/allparks/{redirect_name}"
-        r = requests.get(url)
+        r = requests.get(url, verify=False)
         soup = BeautifulSoup(r.content, features="html.parser")
 
     # print(soup)
@@ -30,38 +58,36 @@ def get_maps_link_from_page(id):
 
     if link is None:
         print("Could not find a Google Maps link on the page!")
-        missing_map_link.append(url)
-        return None
+        return None, LinkResult.LinkMissing
 
-    return link["href"]
+    return link["href"], LinkResult.Successful
 
 
 def main():
-    with open("data_url.txt", "r") as f:
-        data_url = f.read()
 
-    all_data = requests.get(data_url).json()
-    parks = all_data["features"]
+    parks = get_park_container()
     print(f"Found {len(parks)} parks in the list")
 
     successful_pages = 0
-    with open("google_maps_links.txt", "w") as f:
+    missing_map_link = []
+    with open("google_maps_links.txt", "w", newline="", encoding="utf-8") as f:
         map_link_writer = csv.writer(f)
         for park in parks:
-            park_name = park["attributes"]["PMA_Name"]
-            park_id = park["attributes"]["PMA"]
+            park_name, park_id, url = get_park_info(park)
             print(
                 "--------------------------------------------------------------------"
             )
             print(f"Trying {park_name}, {park_id}")
             try:
-                map_link = get_maps_link_from_page(park_id)
+                map_link, result = get_maps_link_from_page(url)
             except Exception as e:
                 print(f"Failed to parse {park_name}, {park_id}")
                 raise e
 
             if map_link is None:
                 print(f"Failed to find valid page for {park_name}")
+                if result == LinkResult.LinkMissing:
+                    missing_map_link.append(url)
                 continue
 
             print(f"Map link for {park_name}: {map_link}")
